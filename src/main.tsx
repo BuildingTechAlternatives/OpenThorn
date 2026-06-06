2b4cc5efa41c3bb47f06e51b3060afc6833a4cbf
Thomas Tschinkel <Thomas.Tschinkel01@gmail.com>
2026-06-06 16:45:24 +0200

    feat: add CookieBanner with consent-gated PostHog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,10 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { SupabaseAuthProvider } from './lib/AuthContext'
 import { initSentry } from './lib/sentry'
-import { initAnalytics } from './lib/analytics'
 import App from './App'
 import './index.css'
 
 initSentry()
-initAnalytics()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
